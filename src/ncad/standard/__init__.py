ae097788faa94a0c49d57ff00fa5d5c71e6df783
deepsaia <deepak.singh10@cognizant.com>
2026-07-21 12:51:09 +0530

    standard: register the five new families in the library facade

--- src/ncad/standard/__init__.py
+++ src/ncad/standard/__init__.py
@@ -1,8 +1,16 @@
 """Standard-part generators: designation -> a buildable ncad part document, generated natively."""
 
+from ncad.standard.bearing_generator import BearingGenerator
+from ncad.standard.flange_generator import FlangeGenerator
+from ncad.standard.gasket_generator import GasketGenerator
 from ncad.standard.hex_nut_generator import HexNutGenerator
+from ncad.standard.i_beam_generator import IBeamGenerator
+from ncad.standard.pipe_generator import PipeGenerator
 from ncad.standard.standard_library import StandardLibrary
 from ncad.standard.standard_table import StandardTable
 from ncad.standard.washer_generator import WasherGenerator
 
-__all__ = ["HexNutGenerator", "StandardLibrary", "StandardTable", "WasherGenerator"]
+__all__ = [
+    "BearingGenerator", "FlangeGenerator", "GasketGenerator", "HexNutGenerator",
+    "IBeamGenerator", "PipeGenerator", "StandardLibrary", "StandardTable", "WasherGenerator",
+]
